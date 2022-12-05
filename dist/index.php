2df05b1b6cc8b477aa6e8d0dcb876e1a30a16b21
Ismail Bourbie <bourbieismail@gmail.com>
2022-12-05 14:35:16 +0100

    added dependencies, working on routing

--- dist/index.php
+++ dist/index.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Ismail Bourbie</title>
-</head>
-<body>
-    Hello world
-</body>
-</html>
+<?php
+
+$url =  '/' . trim($_SERVER['REQUEST_URI'], '/');
+
+$uri = parse_url($url, PHP_URL_PATH);
+// require_once($file);
+$routes = [
+    "/" => "home.php",
+    "/home" => "home.php",
+    "/about" => "about.php",
+    "/work" => "work.php",
+    "/contact" => "contact.php"
+];
+
+if (key_exists($uri, $routes))
+    echo $routes[$uri];
+else
+    echo '404 - Not Found';
