71b76c0bb46c3e354211df72bd71ef22698d4d4f
Ismail Bourbie <bourbieismail@gmail.com>
2022-12-05 15:03:28 +0100

    added includes, ready to go

--- src/views/about.php
+++ src/views/about.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 - Not Found</title>
-</head>
-<body>
-    <h1>404 - Page not found</h1>
-</body>
-</html>
+<?php include 'includes/header.php'; ?>
+
+<h1 class="text-3xl text-red-400">About page</h1>
+
+<?php include 'includes/footer.php'; ?>
--- src/views/home.php
+++ src/views/home.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <title>Home - Ismail Bourbie</title>
-</head>
-<body>
-    Home page
-</body>
-</html>
+<?php include 'includes/header.php'; ?>
+Home page
+<?php include 'includes/footer.php'; ?>
