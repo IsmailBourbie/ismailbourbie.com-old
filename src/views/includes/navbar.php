8b8ef1d70b5bba42678226f3c632131bb253c9d3
Ismail Bourbie <bourbieismail@gmail.com>
2022-12-07 15:26:55 +0100

    work with alpine, added animation

--- src/views/includes/navbar.php
+++ src/views/includes/navbar.php
@@ -1,49 +1,49 @@
-<nav>
+<nav x-data="{ open: false }">
     <div class="relative flex justify-between items-center px-6 py-4 z-50">
-        <button id="menuButton" class="text-gray-500 w-10 h-10 relative focus:outline-none">
+        <button class="text-gray-500 w-10 h-10 relative focus:outline-none" @click="open = !open">
             <span class="sr-only">Open main menu</span>
             <div class="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
-                <span aria-hidden="true" class="block absolute h-0.5 w-5 bg-black -translate-y-1.5 transform transition duration-500 ease-in-out"></span>
-                <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-black   transform transition duration-500 ease-in-out"></span>
-                <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-black translate-y-1.5 transform  transition duration-500 ease-in-out"></span>
+                <span aria-hidden="true" class="block absolute h-0.5 w-5 bg-black transform transition duration-500 ease-in-out" :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
+                <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-black   transform transition duration-500 ease-in-out" :class="{'opacity-0': open } "></span>
+                <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-black transform  transition duration-500 ease-in-out" :class="{'-rotate-45': open, ' translate-y-1.5': !open}"></span>
             </div>
         </button>
         <div class="flex items-center">
             <img src="/images/icons/contact.svg" alt="contact" class="inline mr-1"> <span class="hidden md:inline text-lg text-black tracking-wide">Say Hello</span>
         </div>
     </div>
-    <div id="menu" class="fixed transform -translate-y-full transition duration-500 inset-0 z-40 bg-yellow-100 border-8 border-white pt-8 md:pt-20 pb-4">
+    <div class="fixed transform transition duration-500 inset-0 z-40 -translate-y-full bg-yellow-100 border-8 border-white pt-8 md:pt-20 pb-4" :class="{'translate-y-0': open}">
         <div class="container mx-auto h-full">
             <div class="flex flex-col justify-between h-full">
                 <div class="flex flex-1 flex-col md:flex-row w-full px-6 md:px-20">
                     <ul class="flex flex-col flex-1 justify-evenly md:justify-around text-center md:text-left">
-                        <li class="text-6xl font-bold text-orange-600"><a href="#">Home</a></li>
-                        <li class="text-6xl font-bold text-orange-600"><a href="#">Profile</a></li>
-                        <li class="text-6xl font-bold text-orange-600"><a href="#">About</a></li>
-                        <li class="text-6xl font-bold text-orange-600"><a href="#">Contact</a></li>
+                        <li class="text-6xl font-bold text-orange-600" x-show="open" x-transition:enter.delay.300ms.duration.800ms><a href="#">Home</a></li>
+                        <li class="text-6xl font-bold text-orange-600" x-show="open" x-transition:enter.delay.300ms.duration.1100ms><a href="#">Profile</a></li>
+                        <li class="text-6xl font-bold text-orange-600" x-show="open" x-transition:enter.delay.300ms.duration.1300ms><a href="#">About</a></li>
+                        <li class="text-6xl font-bold text-orange-600" x-show="open" x-transition:enter.delay.300ms.duration.1600ms><a href="#">Contact</a></li>
                     </ul>
                     <ul class="flex flex-row md:flex-col justify-between items-center md:py-40 ">
-                        <li class="">
+                        <li class="" x-show="open" x-transition:enter.delay.300ms.duration.800ms>
                             <a class="menuItem" href="#">
                                 <img src="/images/icons/twitter.svg" alt="twitter">
                             </a>
                         </li>
-                        <li class="">
+                        <li class="" x-show="open" x-transition:enter.delay.300ms.duration.1100ms>
                             <a class="menuItem" href="#">
                                 <img src="/images/icons/instagram.svg" alt="instagram">
                             </a>
                         </li>
-                        <li class="">
+                        <li class="" x-show="open" x-transition:enter.delay.300ms.duration.1300ms>
                             <a class="menuItem" href="#">
                                 <img src="/images/icons/github.svg" alt="github">
                             </a>
                         </li>
-                        <li class="">
+                        <li class="" x-show="open" x-transition:enter.delay.300ms.duration.1600ms>
                             <a class="menuItem" href="#">
                                 <img src="/images/icons/linkedin.svg" alt="linkedin">
                             </a>
                         </li>
-                        <li class="">
+                        <li class="" x-show="open" x-transition:enter.delay.300ms.duration.1900ms>
                             <a class="menuItem" href="#">
                                 <img src="/images/icons/mail.svg" alt="mail">
                             </a>
@@ -51,7 +51,7 @@
                     </ul>
 
                 </div>
-                <p class="translate-y-0 text-center border-t border-main text-main py-2 md:py-5 w-11/12 md:w-10/12 mx-auto mt-8">© Ismail Bourbie 2022. All Rights Reserved.</p>
+                <p class="translate-y-0 text-center border-t border-main text-main py-2 md:py-5 w-11/12 md:w-10/12 mx-auto mt-8" x-show="open" x-transition:enter.delay.300ms>© Ismail Bourbie 2022. All Rights Reserved.</p>
             </div>
         </div>
     </div>
