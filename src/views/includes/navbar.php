b7a2ecd11eba40da917c160c215c2171eeda3b6b
Ismail Bourbie <bourbieismail@gmail.com>
2022-12-07 22:09:43 +0100

    edited responsive on Tablets size

--- src/views/includes/navbar.php
+++ src/views/includes/navbar.php
@@ -9,20 +9,20 @@
             </div>
         </button>
         <div class="flex items-center">
-            <img src="/images/icons/contact.svg" alt="contact" class="inline mr-1"> <span class="hidden md:inline text-lg text-black tracking-wide">Say Hello</span>
+            <img src="/images/icons/contact.svg" alt="contact" class="inline mr-1"> <span class="hidden lg:inline text-lg text-black tracking-wide">Say Hello</span>
         </div>
     </div>
-    <div class="fixed transform transition duration-500 inset-0 z-40 -translate-y-full bg-yellow-100 border-8 border-white pt-8 md:pt-20 pb-4" :class="{'translate-y-0': open}">
+    <div class="fixed transform transition duration-500 inset-0 z-40 -translate-y-full bg-yellow-100 border-8 border-white pt-8 lg:pt-20 pb-4" :class="{'translate-y-0': open}">
         <div class="container mx-auto h-full">
             <div class="flex flex-col justify-between h-full">
-                <div class="flex flex-1 flex-col md:flex-row w-full px-6 md:px-20">
-                    <ul class="flex flex-col flex-1 justify-evenly md:justify-around text-center md:text-left">
+                <div class="flex flex-1 flex-col lg:flex-row w-full px-6 lg:px-20">
+                    <ul class="flex flex-col flex-1 justify-evenly lg:justify-around text-center lg:text-left">
                         <li class="text-6xl font-bold text-orange-600" x-show="open" x-transition:enter.delay.300ms.duration.800ms><a href="#">Home</a></li>
                         <li class="text-6xl font-bold text-orange-600" x-show="open" x-transition:enter.delay.300ms.duration.1100ms><a href="#">Profile</a></li>
                         <li class="text-6xl font-bold text-orange-600" x-show="open" x-transition:enter.delay.300ms.duration.1300ms><a href="#">About</a></li>
                         <li class="text-6xl font-bold text-orange-600" x-show="open" x-transition:enter.delay.300ms.duration.1600ms><a href="#">Contact</a></li>
                     </ul>
-                    <ul class="flex flex-row md:flex-col justify-between items-center md:py-40 ">
+                    <ul class="flex flex-row lg:flex-col justify-between md:justify-around lg:justify-between items-center lg:py-40 ">
                         <li class="" x-show="open" x-transition:enter.delay.300ms.duration.800ms>
                             <a class="menuItem" href="#">
                                 <img src="/images/icons/twitter.svg" alt="twitter">
@@ -51,7 +51,7 @@
                     </ul>
 
                 </div>
-                <p class="translate-y-0 text-center border-t border-main text-main py-2 md:py-5 w-11/12 md:w-10/12 mx-auto mt-8" x-show="open" x-transition:enter.delay.300ms>© Ismail Bourbie 2022. All Rights Reserved.</p>
+                <p class="translate-y-0 text-center border-t border-main text-main py-2 lg:py-5 w-11/12 lg:w-10/12 mx-auto mt-8" x-show="open" x-transition:enter.delay.300ms>© Ismail Bourbie 2022. All Rights Reserved.</p>
             </div>
         </div>
     </div>
