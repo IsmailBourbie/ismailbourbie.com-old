9d52bdcae357f02c0ee29d1a8a6c3925f06bdf06
Ismail Bourbie <bourbieismail@gmail.com>
2022-12-15 15:14:28 +0100

    added used technologies

--- src/views/about.php
+++ src/views/about.php
@@ -6,23 +6,22 @@
             <img class="max-w-lg rounded-lg after:border-red-600" src="https://via.placeholder.com/271x380" alt="just a selfie">
         </div>
         <div class="lg:mx-5 xl:mx-8">
-            <h1 class="flex items-center mb-4 text-sm font-bold text-orange-600 tracking-widest after:flex-1 after:ml-2 after:h-[1px] after:w-44 after:bg-main">A BIT ABOUT ME</h1>
-            <p class="my-2 tracking-wider text-zinc-500">
+            <h1 class="flex items-center mb-4 text-sm font-bold text-orange-600 tracking-widest after:flex-1 after:ml-2 after:h-[1px] after:w-44 after:bg-gray-300">A BIT ABOUT ME</h1>
+            <p class="my-2 tracking-wider text-slate-500">
                 Hello! My name is Ismail and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
             </p>
-            <p class="my-2 tracking-wider text-zinc-500">
+            <p class="my-2 tracking-wider text-slate-500">
                 I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
             </p>
-            <p class="my-2 tracking-wider text-zinc-500">
+            <p class="my-2 tracking-wider text-slate-500">
                 Here are a few technologies I’ve been working with recently:
             </p>
-            <ul class="pt-2 pb-4 md:pb-0 flex flex-row flex-wrap"  x-data="{tags: 6}">
-                <template x-for="i in tags">
-                    <li class="basis-1/2 md:basis-1/3 relative pl-5 before:content-['\2023'] before:text-4xl before:leading-5 before:text-orange-600 before:absolute before:left-0" x-text="'PHP Language'"></li>
+            <ul class="pt-2 pb-4 md:pb-0 flex flex-row flex-wrap"  x-data="{technologies: ['PHP', 'Javascript', 'Tailwindcss', 'Laravel', 'Vue.js', 'Alpine.js']}">
+                <template x-for="tech in technologies">
+                    <li class="basis-1/2 md:basis-1/3 text-slate-400 relative pl-5 before:content-['\2023'] before:text-4xl before:leading-5 before:text-orange-600 before:absolute before:left-0" x-text="tech"></li>
                 </template>
             </ul>
         </div>
     </div>
 </div>
-
 <?php include 'includes/footer.php'; ?>
