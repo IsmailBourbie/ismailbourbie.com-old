b6688d2f599ea807c438fe42f688eb2093668b4d
Ismail Bourbie <bourbieismail@gmail.com>
2023-01-10 15:02:07 +0100

    fixing lighthous issues

--- src/views/contact.php
+++ src/views/contact.php
@@ -4,26 +4,26 @@
         <img src="/images/contact_box.svg" alt="contact" class="max-w-lg">
     </div>
     <div class="w-full">
-        <h2 class="text-orange-600 border-b-4 border-main pb-2">Need a hand? Here’s how to reach me.</h2>
+        <h1 class="text-orange-600 text-xl border-b-4 border-main pb-2">Need a hand? Here’s how to reach me.</h1>
         <ul class="mt-5 flex flex-col lg:flex-row justify-between">
             <li class="mb-5">
                 <div class="flex items-center">
                     <img src="/images/icons/home.svg" alt="contact" class="inline mr-1">
-                    <h4 class="text-main tracking-wider">Location</h4>
+                    <h2 class="text-main tracking-wider">Location</h2>
                 </div>
                 <p class="font-bold text-lg">Oued Tolba City, Tiaret Algeria.</p>
             </li>
             <li class="mb-5">
                 <div class="flex items-center">
                     <img src="/images/icons/mail.svg" alt="contact" class="inline mr-1 w-4">
-                    <h4 class="text-main tracking-wider">Email</h4>
+                    <h2 class="text-main tracking-wider">Email</h2>
                 </div>
                 <p class="font-bold text-lg">bourbieismail@gmail.com</p>
             </li>
             <li class="mb-5">
                 <div class="flex items-center">
                     <img src="/images/icons/phone.svg" alt="contact" class="inline mr-1">
-                    <h4 class="text-main tracking-wider">Phone</h4>
+                    <h2 class="text-main tracking-wider">Phone</h2>
                 </div>
                 <p class="font-bold text-lg">+213 662 969 911</p>
             </li>
