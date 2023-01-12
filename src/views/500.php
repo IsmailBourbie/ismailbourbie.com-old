d1360013a26e457529024763d6d184cbb7e26ffb
Ismail Bourbie <bourbieismail@gmail.com>
2023-01-12 14:18:46 +0100

    added 500 page

--- src/views/500.php
+++ src/views/500.php
@@ -1,3 +1,7 @@
 <?php include 'includes/header.php'; ?>
-<p>Ooops something went wrong, please report this on <u>bourbieismail@gmail.com</u></p>
+<div class="container px-6 flex flex-col justify-center items-center flex-1 text-center">
+    <h1 class="text-9xl text-orange-600">500</h1>
+    <h2 class="font-bold">Oops! Something went wrong.</h2>
+    <p class="text-sm my-4">We're experiencing an internal server problem. Please try again later or contact <a href="mailto:bourbieismail@gmail.com?subject=500 Internal server error" class="text-orange-600 underline">bourbieismail@gmail.com</a></p>
+</div>
 <?php include 'includes/footer.php'; ?>
