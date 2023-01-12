f90979edd74ad611255dc3d706871b6b7dcf5e81
Ismail Bourbie <bourbieismail@gmail.com>
2023-01-12 14:10:02 +0100

    added 404 page

--- src/views/404.php
+++ src/views/404.php
@@ -1,3 +1,7 @@
 <?php include 'includes/header.php'; ?>
-    <h1>404 - Page not found</h1>
+<div class="container px-6 flex flex-col justify-center items-center flex-1 text-center">
+    <h1 class="text-9xl text-orange-600">404</h1>
+    <h2 class="font-bold">Oops! That page can’t be found.</h2>
+    <p class="text-sm my-4">It looks like nothing was found at this location. Let's get you <a href="/" class="text-orange-600 underline">back</a></p>
+</div>
 <?php include 'includes/footer.php'; ?>
